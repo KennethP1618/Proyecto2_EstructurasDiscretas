42963c772f2f18fee3cafa69d6f1ca3a28f4fcb4
kennethp <kennethpcode@gmail.com>
2026-05-05 15:13:28 -0500

    Creación del módulo de detección de intrusos V.1.0

--- Proyecto2.py
+++ Proyecto2.py
@@ -9,3 +9,11 @@ servidorCritico = {"192.168.1.10", "10.0.0.50", "192.168.1.55",
 #Lista negra(IPs maliciosas)
 listaNegra ={"200.50.10.1", "172.16.10.10", "192.168.1.99", 
              "10.0.0.51", "8.8.8.8"}
+#Módulo Detección de intrusos
+def deteccionIntrusos():
+    intruso = servidorCritico & listaNegra & vpn_autorizado
+    print(f"IPs en riesgo alto: {len(intruso)}")
+    print(intruso)
+#Testeo del módulo
+os.system("cls")
+deteccionIntrusos()
